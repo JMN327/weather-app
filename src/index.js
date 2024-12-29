bcb178218e90a521bdb8057a7a2693e6c1312dd6
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-12-29 15:42:19 +0000

    Up to 2. of assignment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,49 @@ import ClassTemplate from "./class-template.js";
 console.log("Hello World!)");
 console.log(`Storage available: ${storageAvailable("localStorage")}`);
 
-const x = new ClassTemplate({ exposedVariable1: "mishmash",  });
-x.exposedVariable2 = 7;
-console.log(x.exposedVariable2, x.exposedVariable1);
+
+
+const img = document.querySelector("img");
+const btn = document.querySelector("#btn");
+let search = document.querySelector("#search");
+btn.addEventListener("click", (event) => testing(event));
+btn.addEventListener("click", fetchNewPic);
+
+function fetchNewPic() {
+  console.log("fetching pic...");
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?key=TNK3W4F4DRB846HC8URN7GCNE`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      //img.style.display = "block";
+      //img.src = response.data.images.original.url;
+      console.log(response)
+    });
+}
+
+function testing(event) {
+  console.log("test:", search.value);
+}
+
+async function fetchNewPicAA() {
+  console.log("fetching pic async...");
+
+  try {
+    const responsePromise = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?key=TNK3W4F4DRB846HC8URN7GCNE`,
+      { mode: "cors" }
+    );
+    const response = await responsePromise.json();
+    img.style.display = "block";
+    img.src = response.data.images.original.url;
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+
