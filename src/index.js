ab49768fc108919c64942b1649d6cfb41740b3af
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-12-31 15:30:27 +0000

    Start work on display

--- src/index.js
+++ src/index.js
@@ -1,54 +1,43 @@
 import "./styles.css";
 import storageAvailable from "./local-storage.js";
-import ClassTemplate from "./class-template.js";
+//import ClassTemplate from "./class-template.js";
+import gifCloudy from "./gifs/cloudy.gif";
+import gifClearDay from "./gifs/clear-day.gif";
+import gifClearNight from "./gifs/clear-night.gif";
+import gifFog from "./gifs/fog.gif";
+import gifPartlyCloudyDay from "./gifs/partly-cloudy-day.gif";
+import gifPartlyCloudyNight from "./gifs/partly-cloudy-night.gif";
+import gifRain from "./gifs/rain.gif";
+import gifSnow from "./gifs/snow.gif";
+import gifWind from "./gifs/wind.gif";
 
 console.log("Hello World!)");
 console.log(`Storage available: ${storageAvailable("localStorage")}`);
 
-
-
-const img = document.querySelector("img");
 const btn = document.querySelector("#btn");
 let search = document.querySelector("#search");
 let dateBox = document.querySelector("#date1");
+let resultContents = document.querySelector(".result-contents");
 btn.addEventListener("click", (event) => testing(event));
-btn.addEventListener("click", fetchNewPic);
+btn.addEventListener("click", fetchWeather);
 
-function fetchNewPic() {
-  console.log("fetching weather...");
-  let searchString
-  console.log("date:" + dateBox.value, !dateBox.value)
-  if (dateBox.value) {
-    searchString =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}/${dateBox.value}?key=TNK3W4F4DRB846HC8URN7GCNE`
-  } else {
-    searchString =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?key=TNK3W4F4DRB846HC8URN7GCNE&iconSet=icons1`
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
-  console.log(searchString)
-  fetch(
-    searchString,
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      //img.style.display = "block";
-      //img.src = response.data.images.original.url;
-      console.log(dayData(response))
-    });
 }
 
 function dayData(APIResponse) {
-  const conditions = APIResponse.days.map((day) =>(day.conditions))
-  const description = APIResponse.days.map((day)=>(day.description))
-  const icon = APIResponse.days.map((day)=>(day.icon))
-  const temp = APIResponse.days.map((day)=>(day.temp))
-  const tempmax = APIResponse.days.map((day)=>(day.tempmax))
-  const tempmin = APIResponse.days.map((day)=>(day.tempmin))
-  const winddir = APIResponse.days.map((day)=>(day.winddir))
-  const windspeed = APIResponse.days.map((day)=>(day.windspeed))
-  const sunrise = APIResponse.days.map((day)=>(day.sunrise))
-  const sunset = APIResponse.days.map((day)=>(day.sunset))
+  const conditions = APIResponse.days.map((day) => day.conditions);
+  const description = APIResponse.days.map((day) => day.description);
+  const icon = APIResponse.days.map((day) => day.icon);
+  const temp = APIResponse.days.map((day) => day.temp);
+  const tempmax = APIResponse.days.map((day) => day.tempmax);
+  const tempmin = APIResponse.days.map((day) => day.tempmin);
+  const winddir = APIResponse.days.map((day) => day.winddir);
+  const windspeed = APIResponse.days.map((day) => day.windspeed);
+  const sunrise = APIResponse.days.map((day) => day.sunrise);
+  const sunset = APIResponse.days.map((day) => day.sunset);
 
   return {
     conditions,
@@ -60,30 +49,83 @@ function dayData(APIResponse) {
     winddir,
     windspeed,
     sunrise,
-    sunset
-  }
-
+    sunset,
+  };
 }
 
 function testing(event) {
   console.log("test:", search.value);
 }
 
-async function fetchNewPicAA() {
+async function fetchWeather() {
   console.log("fetching pic async...");
 
   try {
-    const responsePromise = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?key=TNK3W4F4DRB846HC8URN7GCNE`,
-      { mode: "cors" }
-    );
+    let searchString;
+    console.log("date:" + dateBox.value, !dateBox.value);
+    if (dateBox.value) {
+      searchString = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}/${dateBox.value}?key=TNK3W4F4DRB846HC8URN7GCNE`;
+    } else {
+      searchString = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?key=TNK3W4F4DRB846HC8URN7GCNE&iconSet=icons1`;
+    }
+    const responsePromise = await fetch(searchString, { mode: "cors" });
     const response = await responsePromise.json();
-    img.style.display = "block";
-    img.src = response.data.images.original.url;
+    displayWeather(response);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
 }
 
+function addElement(tag = "div", classes = [], parent = null) {
+  let element = document.createElement(tag);
+  element.classList.add(classes);
+  if (parent) {
+    parent.appendChild(element);
+  }
+  return element;
+}
 
+function displayWeather(response) {
+  removeAllChildNodes(resultContents);
+  let data = dayData(response);
+  let day = addElement("div", "day", resultContents);
+  let icon = addElement("img", "day-icon", resultContents);
+  icon.src = getIcon(data.icon[0]);
+  icon.style.width = "6rem";
+  icon.style.aspectRatio = 1;
+  day.textContent = data.description[0];
+}
+
+function getIcon(iconData) {
+  switch (iconData) {
+    case "cloudy":
+      return gifCloudy;
+      break;
+    case "clear-day":
+      return gifClearDay;
+      break;
+    case "clear-night":
+      return gifClearNight;
+      break;
+    case "fog":
+      return gifFog;
+      break;
+    case "partly-cloudy-day":
+      return gifPartlyCloudyDay;
+      break;
+    case "partly-cloudy-night":
+      return gifPartlyCloudyNight;
+      break;
+    case "rain":
+      return gifRain;
+      break;
+    case "snow":
+      return gifSnow;
+      break;
+    case "wind":
+      return gifWind;
+      break;
+    default:
+      break;
+  }
+}
